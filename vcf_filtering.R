897036fb6d5b1437267b6a48c8f004ac9c89fd99
elakatos <e.lakatos@qmul.ac.uk>
2018-03-07 10:22:29 +0000

    Added vcf filtering for IBD data

---
+++ vcf_filtering.R
@@ -0,0 +1,39 @@
+normInd = 10
+setwd('~/RNAseq/Neoepitopes/')
+
+fillNormal <- function(x, normInd, altnormInd){
+  if (!is.na(altnormInd)) {
+    for (ind in altnormInd){
+      x[x[,normInd]=='.', normInd] <- x[x[,normInd]=='.', ind]
+    }
+    x <- x[, -(altnormInd)] #Delete alternative normal columns
+  }
+  
+  return(x)
+}
+
+filterSomatic <- function(vcf, normInd){
+  normalAD <- sapply(vcf[,normInd], function(x) tail(unlist(strsplit(x, ':')),1)) #get AD field of normal
+  splitAD <- strsplit(normalAD, ',') #Split it to ref and alt
+  
+  vcf.somatic <- vcf[((map(splitAD,1)>99) | (map(splitAD,2)==0)),]
+  return(vcf.somatic)
+}
+
+fileList <- read.table('IBD_vcf_list.txt', header=T,stringsAsFactors = F, sep='\t')
+
+
+for (i in 1:(nrow(fileList))){
+altnormInd <- as.numeric(unlist(strsplit(fileList[i,,drop=F]$altnormInd,',')))
+fileName <- fileList[i,,drop=F]$Sample
+print(fileName)
+
+vcf <- read.table(fileName, header=T, sep='\t', stringsAsFactors = F)
+
+vcf.pass <- vcf[vcf$FILTER=='PASS',] #Only with PASS flag
+vcf.singlealt <- vcf.pass[!grepl(',',vcf.pass$ALT),] #Only with single ALT allele
+vcf.filled <- fillNormal(vcf.singlealt, normInd, altnormInd) #Unite normal information in one column
+vcf.somatic <- filterSomatic(vcf.filled, normInd)
+
+write.table(vcf.somatic, file=paste0(fileName,'.somatic'), sep='\t', row.names=F, quote=F)
+}
