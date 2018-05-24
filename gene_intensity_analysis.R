97d86d9d5940f5095ae7fb85f747057ce1340533
elakatos <e.lakatos@qmul.ac.uk>
2018-05-24 17:32:13 +0100

    Gene intensity

--- gene_intensity_analysis.R
+++ gene_intensity_analysis.R
@@ -1,4 +1,45 @@
+
+# Helper functions --------------------------------------------------------
+
+getGeneScores <- function(epTable, geneTable){
+  altNameTable <- data.frame(matrix(vector(), ncol=1)); names(altNameTable) <- 'Alt.name'
+  
+  exprTable <- data.frame(matrix(0, ncol=length(unique(epTable$Sample)), nrow=length(unique(geneTable$Gene.name))))
+  names(exprTable) <- unique(epTable$Sample)
+  row.names(exprTable) <- unique(geneTable$Gene.name)
+  
+  for (i in row.names(epTable)){
+    samp <- epTable[i, 'Sample']
+    x <- epTable[i,]
+    if (x$Gene.name %in% row.names(exprTable)){
+      #We can use Gene.name as row-identifier
+      gN <- x$Gene.name
+    }else{
+      if (x$Gene.name %in% row.names(altNameTable)){
+        #we already came across this and use the recorded row-identifier
+        gN <- altNameTable[x$Gene.name,'Alt.name']
+      }else{
+        #we identify the change by chromosome and position
+        gene <- subset(geneTable, (Chrom == x$Chrom) & (Start < x$Start) & (End > x$Start) )
+        if (nrow(gene)>=1){
+          gN <- as.character(gene$Gene.name)[1]
+          altNameTable[x$Gene.name,] <- gN
+        }else{cat("No match found for gene ", x$Gene.name,", omitting from analysis.\n", sep='')
+          next}
+      }
+    }
+    exprTable[gN,samp] <- exprTable[gN,samp]+ epTable[i, 'ImmuneScore'] 
+  }
+  return(exprTable)
+}
+
+
+
+# Analysis of CRCmseq -----------------------------------------------------
+
+
 geneTable <- read.table('~/Dropbox/Code/mart_export_hg19.txt', sep='\t', header=T)
+names(geneTable) <- c('Gene.ID', 'Start', 'End', 'Chrom', 'Gene.name')
 
 dir <- '~/CRCdata/CRCmseq_Set'
 epTable <- read.table(paste0(dir, '/Neopred_results/Output_BA.neoantigens.txt'), header=F,
@@ -9,53 +50,16 @@ names(epTable) <- c('Sample', getRegionNames(ncol(epTable)-24), 'LineID', 'Chrom
 epNon <- epTable[epTable$Novelty==0,]
 epTable <- epTable[epTable$Novelty!=0,]
 
+#Plot score distribution for a selected sample
 sample <- 'Set.04.snv'
 eps <- subsetEpTable(epTable, sample)
-
 ggplot(eps, aes(x = Score)) + geom_histogram()
 
 
-
-# Assign score to genes ---------------------------------------------------
-
-epTable$Gene.name <- sapply(epTable$Gene, function(x) unlist(strsplit(x, ':'))[1]  )
+#Create immunescore
 epTable$ImmuneScore <- epTable$Score
 
+#Assign score to genes
+epTable$Gene.name <- sapply(epTable$Gene, function(x) unlist(strsplit(x, ':'))[1]  )
 exprTable <- getGeneScores(epTable, geneTable)
 exprTableFiltered <- exprTable[rowSums(exprTable > 0)>2,]
-
-
-
-
-
-getGeneScores <- function(epTable, geneTable){
-  altNameTable <- data.frame(matrix(vector(), ncol=1)); names(altNameTable) <- 'Alt.name'
-  
-  exprTable <- data.frame(matrix(0, ncol=length(unique(epTable$Sample)), nrow=length(unique(geneTable$Gene.name))))
-  names(exprTable) <- unique(epTable$Sample)
-  row.names(exprTable) <- unique(geneTable$Gene.name)
-  
-for (i in row.names(epTable)){
-  samp <- epTable[i, 'Sample']
-  x <- epTable[i,]
-  if (x$Gene.name %in% row.names(exprTable)){
-    #We can use Gene.name as row-identifier
-    gN <- x$Gene.name
-  }else{
-    if (x$Gene.name %in% row.names(altNameTable)){
-      #we already came across this and use the recorded row-identifier
-      gN <- altNameTable[x$Gene.name,'Alt.name']
-    }else{
-      #we identify the change by chromosome and position
-      gene <- subset(geneTable, (Chrom == x$Chrom) & (Start < x$Start) & (End > x$Start) )
-      if (nrow(gene)==1){
-        gN <- as.character(gene$Gene.name)
-        altNameTable[x$Gene.name,] <- gN
-      }else{cat("No or unambiguous match found for gene ", x$Gene.name,", omitting from analysis.\n")
-        next}
-    }
-  }
-  exprTable[gN,samp] <- exprTable[gN,samp]+ epTable[i, 'ImmuneScore'] 
-}
-  return(exprTable)
-}
